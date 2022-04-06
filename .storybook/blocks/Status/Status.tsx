6547a481df171f862134503a83f77950ee2cbc5b
mwagdi <mahmoud.elawadi@hey.car>
2022-04-06 15:16:09 +0200

    fix fix type issue on Status

--- .storybook/blocks/Status/Status.tsx
+++ .storybook/blocks/Status/Status.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const STATUS = {
-  1: 'Draft',
-  2: 'Candidate',
+  0: 'Proposal',
+  1: 'In Development',
+  2: 'In Review',
   3: 'Recommendation',
 };
 
@@ -10,12 +11,14 @@ type StatusProps = {
   stage: number;
 };
 
+type Index = keyof typeof STATUS;
+
 export const Status: React.FC<StatusProps> = ({ stage }) => (
-  <div>
-    <a
-      href="./?path=/docs/guidelines-component-statuses--page"
-    >
-      {STATUS[stage]}
-    </a>
-  </div>
+    <div>
+      <a
+        href="./?path=/docs/guidelines-component-statuses--page"
+      >
+        {STATUS[stage as Index]}
+      </a>
+    </div>
 );
