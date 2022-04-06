fc44d525bc36e05a92b37a8b67607bd9f33ef5d9
mwagdi <mahmoud.elawadi@hey.car>
2022-04-06 15:19:26 +0200

    feat: style table

--- .storybook/blocks/Table/Table.tsx
+++ .storybook/blocks/Table/Table.tsx
@@ -1,12 +1,14 @@
 import React, { ReactNode } from 'react';
 
+import styles from './Table.module.css';
+
 type TableProps = {
   header: ReactNode[];
   rows: ReactNode[][];
 };
 
 export const Table = ({ header, rows }: TableProps): JSX.Element => (
-  <table>
+  <table className={styles.table}>
     <thead>
       {header.map(cell => <th>{cell}</th>)}
     </thead>
