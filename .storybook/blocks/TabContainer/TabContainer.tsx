5d48c3c588466fcab5e539af7c16e118356401d2
mwagdi <mahmoud.elawadi@hey.car>
2022-04-12 09:00:52 +0200

    refactor: define button attributes

--- .storybook/blocks/TabContainer/TabContainer.tsx
+++ .storybook/blocks/TabContainer/TabContainer.tsx
@@ -2,17 +2,33 @@ import React, { useState } from 'react';
 
 import styles from './TabContainer.module.css';
 import { TabContainerProps } from './TabContainer.types';
+import cn from 'classnames';
 
-export const TabContainer: React.FC<TabContainerProps> = ({tabs}) => {
+export const TabContainer: React.FC<TabContainerProps> = ({ tabs }) => {
   const [viewedTab, setViewedTab] = useState<number>(0);
   const isActive = (index: number): boolean => index === viewedTab;
+
+  const getClassName = (index: number): string => cn(isActive(index) && styles.active);
+
+  const handleTabClick = (index: number) => {
+    setViewedTab(index);
+  };
 
   if (!tabs.length) return null;
 
   return (
     <React.Fragment>
       <div className={styles.tab_header}>
-        {tabs.map(({ label }, index) => <button className={isActive(index) && styles.active} onClick={() => setViewedTab(index)}>{label}</button>)}
+        {tabs.map(({ label }, index) => (
+          <button
+            data-index={index}
+            key={`${label}_tab`}
+            className={getClassName(index)}
+            onClick={() => handleTabClick(index)}
+          >
+            {label}
+          </button>
+        ))}
       </div>
       {tabs[viewedTab].component}
     </React.Fragment>
