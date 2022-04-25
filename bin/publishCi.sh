d376a22ed559c2bf1ad97d14232e580238dfbab2
Oleksandr Zhukov <oleksandr.zhukov@heycargroup.com>
2022-04-25 18:00:28 +0200

    build: implement scripts to build release packages
    * The Shell scripts for build releases packages
    * Added the GitHub workflows actions, make releases by the merge to the main branch
    * Added the semantic-release packages as dev-dependencies.
    * Added helper scripts to the package.json file

---
+++ bin/publishCi.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+# Release the root package with semantic-release
+semantic_output=$(npx semantic-release)
+
+# Output log semantics
+echo $semantic_output
+
+# Check that semantic-release has released the root package (I don't know how else to do it yet)
+if [[ $semantic_output =~ "Publishing version" ]]
+then
+    git remote set-url origin https://semantic-release-bot:$GITHUB_TOKEN@github.com/hey-car/hey-ui.git
+    git checkout master
+    git pull origin master --rebase
+    git fetch --tags
+
+    if [ -z $(lerna changed) ]
+    then
+        echo "There are no relevant changes, so no new versions are released."
+    else
+        lerna version --conventional-commits --no-commit-hooks --yes --force-git-tag
+        git push origin master
+        lerna publish from-git --yes
+    fi
+else
+    exit 0
+fi
