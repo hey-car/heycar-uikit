8bb1e0006eb2ce201030b396522519e5aeed9c7d
Oleksandr Zhukov <oleksandr.zhukov@heycargroup.com>
2022-05-27 12:12:43 +0200

    test for button component

--- packages/button/src/__tests__/Button.test.tsx
+++ packages/button/src/__tests__/Button.test.tsx
@@ -6,22 +6,78 @@ import Button from '../Button';
 const dataTestId = 'test-id';
 
 describe('Button', () => {
+  /**
+   * Attributes tests
+   */
+  describe('Attributes tests', () => {
+    it('should set `data-test-id` attribute', () => {
+      const { getByTestId } = render(<Button dataTestId={dataTestId} />);
+
+      expect(getByTestId(dataTestId).tagName).toBe('BUTTON');
+    });
+
+    it('should set default attribute', () => {
+      const { container } = render(<Button disabled={true} />);
+
+      expect(container.firstElementChild).toHaveAttribute('disabled');
+    });
+
+    it('should set type attribute', () => {
+      const type = 'submit';
+      const { container } = render(<Button type={type} />);
+
+      expect(container.firstElementChild).toHaveAttribute('type', type);
+    });
+  });
+
+  /**
+   * Classes tests
+   */
   describe('Classes tests', () => {
+    it('should set `className` class', () => {
+      const className = 'test-class';
+      const { container } = render(<Button className={className} />);
+
+      expect(container.firstElementChild).toHaveClass(className);
+    });
+
     it('should set `size` class', () => {
-      const size = 'middle';
+      const size = 'large';
       const { container } = render(<Button size={size} />);
 
       expect(container.firstElementChild).toHaveClass(size);
     });
 
-    it('should set `variant` class', () => {
-      const view = 'primary';
-      const { container } = render(<Button variant={view} />);
+    it('should set `full width` class', () => {
+      const { container } = render(<Button fullWidth={true} />);
 
-      expect(container.firstElementChild).toHaveClass(view);
+      expect(container.firstElementChild).toHaveClass('fullWidth');
+    });
+
+    it('should set `variant` class', () => {
+      const variant = 'contained';
+      const { container } = render(<Button variant={variant} />);
+
+      expect(container.firstElementChild).toHaveClass(variant);
+    });
+
+    it('should set `color` class', () => {
+      const color = 'primary';
+      const { container } = render(<Button color={color} />);
+
+      expect(container.firstElementChild).toHaveClass(color);
+    });
+
+    it('should set `loading` class', () => {
+      const { container } = render(<Button loading={true} />);
+
+      expect(container.firstElementChild).toHaveClass('loading');
     });
   });
 
+  /**
+   * Callbacks tests
+   */
   describe('Callbacks tests', () => {
     it('should call `onClick` prop', () => {
       const cb = jest.fn();
@@ -60,10 +116,47 @@ describe('Button', () => {
 
       fireEvent.click(buttonNode, { target: buttonNode });
     });
+
+    it('target should contain anchor element', () => {
+      const { getByTestId } = render(
+        <Button dataTestId={dataTestId} href="#" onClick={cb} />,
+      );
+
+      const anchorNode = getByTestId(dataTestId);
+
+      function cb(event: MouseEvent<HTMLAnchorElement>) {
+        expect(event.target).toBe(anchorNode);
+      }
+
+      fireEvent.click(anchorNode, { target: anchorNode });
+    });
+  });
+
+  /**
+   * Custom component
+   */
+  describe('Custom component', () => {
+    it('should use custom component', () => {
+      const cb = jest.fn();
+
+      cb.mockReturnValue(null);
+
+      render(<Button Component={cb} dataTestId={dataTestId} />);
+
+      expect(cb).toBeCalled();
+
+      const props = cb.mock.calls[0][0];
+
+      expect(props['data-test-id']).toBe(dataTestId);
+    });
   });
 
   it('should unmount without errors', () => {
-    const { unmount } = render(<Button>Text</Button>);
+    const { unmount } = render(
+      <Button leftIcon={<span>Left</span>} rightIcon={<span>Right</span>}>
+        Text
+      </Button>,
+    );
 
     expect(unmount).not.toThrowError();
   });
