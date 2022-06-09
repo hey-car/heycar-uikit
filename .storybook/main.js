640e6e2b7c5efe3b89f70df374144e9adf8387ce
mwagdi <mahmoud.elawadi@hey.car>
2022-06-09 11:36:33 +0200

    feat(grid): add viewport addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -37,6 +37,7 @@ module.exports = {
       options: { transcludeMarkdown: true },
     },
     'storybook-addon-live-examples',
+    '@storybook/addon-viewport'
   ],
   webpackFinal: async config => {
     addPackagesDir(config);
