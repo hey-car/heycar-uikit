b3d768ead493f9eae1d932c8d399ac79f5551751
hcafaq <afaq.khan@heycargroup.com>
2022-07-08 00:13:44 +0200

    added css variants

--- packages/logo/src/Logo.types.ts
+++ packages/logo/src/Logo.types.ts
@@ -2,30 +2,3 @@ export type LogoProps = {
   size: number;
   color?: string;
 };
-
-interface FillColor {
-  fill: string;
-  undermark: string;
-}
-
-interface Color {
-  [primary: string]: FillColor;
-  secondary: FillColor;
-  tertiary: FillColor;
-}
-
-export const Colors: Color = {
-  primary: {
-    fill: 'white',
-    undermark: '#00CEA5',
-  },
-  secondary: {
-    fill: '#052962',
-    undermark: '#00CEA5',
-  },
-
-  tertiary: {
-    fill: '#052962',
-    undermark: 'white',
-  },
-};
