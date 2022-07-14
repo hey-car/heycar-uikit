3192053052a782d746ec2d5047ba8b8668e2ea6e
grygoriy <grygoriy.gonchar@hey.car>
2022-07-14 16:19:07 +0200

    MVP implementation // Added screenshot test, updated snapshot

---
+++ packages/switch/src/__tests__/Switch.screenshots.test.tsx
@@ -0,0 +1,24 @@
+import {
+  createStorybookUrl,
+  setupScreenshotTesting,
+} from '../../../screenshotUtils';
+
+const packageName = 'switch';
+
+const clip = { x: 0, y: 0, width: 200, height: 100 };
+const screenshotTesting = setupScreenshotTesting({
+  it,
+  beforeAll,
+  afterAll,
+  expect,
+});
+
+describe('Switch',
+  screenshotTesting({
+    cases: [[
+      'sprite',
+      createStorybookUrl({packageName}),
+    ],],
+    screenshotOpts: { clip },
+  }),
+);
