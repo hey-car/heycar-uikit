60e205578ce2d2024eb22c0a8384a17d7fbe4b91
Oleksandr Zhukov <89923576+derek-heycar@users.noreply.github.com>
2022-07-22 11:57:13 +0200

    feat(icons): the first implementation of the icons package (#40)

---
+++ packages/icons/src/paths/Tick.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import createSvgIcon from '../utils/createSvgIcon';
+
+export const Tick = createSvgIcon(
+  <path d="M21.271 6.13876L10.5086 19.0956L3.30469 12.0918L4.69884 10.6578L10.3521 16.154L19.7325 4.86084L21.271 6.13876Z" />,
+  'Tick',
+);
