fae40014e387fd6a84275fdee825b265b798b7d3
Grygoriy Gonchar <7198161+ggonchar@users.noreply.github.com>
2022-07-27 12:01:03 +0200

    feat(switch): initial implementation of the switch component (#41)

---
+++ packages/switch/src/Switch.types.ts
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export interface SwitchProps {
+  /**
+   * Controls the state of the switch. If true, the switch is displayed as checked
+   */
+  checked: boolean;
+  /**
+   * Function that will be called every time the Switch value is changed. The `boolean` property `event.target.checked` represent the new/target value of the Switch.
+   */
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  /**
+   * Additional `class` names to be added
+   */
+  className?: string;
+  /**
+   * Controls if the Switch is disabled
+   */
+  disabled?: boolean;
+}
