507cfede8b9fad76821638948e1cb0641448630d
Oleksandr Zhukov <89923576+derek-heycar@users.noreply.github.com>
2022-08-11 17:21:18 +0200

    feat(icons): added extra icons
    Icons were added: Facebook,  Instagram, Twitter, LinkedIn, Pinterest, TikTok, Messenger, Youtube,
    Whatsapp, ArrowDown, ArrowTop, ArrowLeft, ArrowRight, ChevronDown, ChevronTop, ChevronLeft,
    ChevronRight, Lock, LockOpen, OfferLeft, OfferRight.

---
+++ packages/icons/src/paths/ArrowLeft.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import createSvgIcon from '../utils/createSvgIcon';
+
+export const ArrowLeft = createSvgIcon(
+  <path
+    clipRule="evenodd"
+    d="M6.82929 13.0009L21 13L20.9999 11L6.82757 11.0009L13.6066 4.22183L12.1924 2.80762L3 12L12.1934 21.1934L13.6076 19.7791L6.82929 13.0009Z"
+    fillRule="evenodd"
+  />,
+  'ArrowLeft',
+);
---
+++ packages/icons/src/paths/ArrowTop.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import createSvgIcon from '../utils/createSvgIcon';
+
+export const ArrowTop = createSvgIcon(
+  <path
+    clipRule="evenodd"
+    d="M10.9996 6.82978L11.0005 21.0005L13.0005 21.0004L12.9996 6.82805L19.7787 13.6071L21.1929 12.1929L12.0005 3.00049L2.80713 12.1938L4.22134 13.6081L10.9996 6.82978Z"
+    fillRule="evenodd"
+  />,
+  'ArrowTop',
+);
