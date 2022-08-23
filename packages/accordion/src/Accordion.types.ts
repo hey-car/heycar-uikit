0d51fa4627b97b36098a3cc8e8e71f906b14462f
Oleksandr Zhukov <89923576+derek-heycar@users.noreply.github.com>
2022-08-23 15:43:03 +0200

    updated tests for accordion

--- packages/accordion/src/Accordion.types.ts
+++ packages/accordion/src/Accordion.types.ts
@@ -1,9 +1,22 @@
-import { AnchorHTMLAttributes } from 'react';
+import React, { AnchorHTMLAttributes } from 'react';
 
 export interface ComponentProps {
+  /**
+   * The content of the button
+   */
   children: React.ReactNode;
+  /**
+   * Title of the accordion
+   */
   title: React.ReactNode;
+  /**
+   * 	If `true`, expands the accordion, otherwise collapse it.
+   *  Setting this prop enables control over the accordion.
+   */
   open?: boolean;
+  /**
+   * If `true`, the component is disabled.
+   */
   disabled?: boolean;
   /**
    * The component used for the root node. Either a string to use a HTML element or a component
@@ -15,4 +28,5 @@ export interface ComponentProps {
   dataTestId?: string;
 }
 
-export type AccordionProps = ComponentProps & AnchorHTMLAttributes<HTMLElement>;
+export type AccordionProps = ComponentProps &
+  Omit<AnchorHTMLAttributes<HTMLElement>, 'title'>;
---
+++ packages/accordion/src/__tests__/Accordion.snapshot.test.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Accordion from '../Accordion';
+
+const accordionTitle = 'title';
+const accordionBodyText =
+  ' You’ll struggle to find any objective reasons to buy a Q3 Sportback over the standard Audi Q3. But, if you want to stand out, the Sportback is';
+
+describe('Accordion Snapshots tests', () => {
+  it('should match snapshot', () => {
+    expect(
+      render(<Accordion title={accordionTitle}>{accordionBodyText}</Accordion>),
+    ).toMatchSnapshot();
+  });
+
+  it('should render accordion title with HTML tag', () => {
+    expect(
+      render(
+        <Accordion title={<h1>{accordionTitle}</h1>}>
+          {accordionBodyText}
+        </Accordion>,
+      ),
+    ).toMatchSnapshot();
+  });
+
+  it('should render accordion `disabled`', () => {
+    expect(
+      render(
+        <Accordion disabled={true} title={accordionTitle}>
+          {accordionBodyText}
+        </Accordion>,
+      ),
+    ).toMatchSnapshot();
+  });
+
+  it('should render accordion `open=true` by default', () => {
+    expect(
+      render(
+        <Accordion open={true} title={accordionTitle}>
+          {accordionBodyText}
+        </Accordion>,
+      ),
+    ).toMatchSnapshot();
+  });
+});
