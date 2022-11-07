494af3a128bebc7f283a67d96b30e650d6588537
Esra Altintas <esra.altintas@heycargroup.com>
2022-11-07 14:09:27 +0100

    feat(switch): add tab index for accessibility with tab key on switch (#95)
    * feat(switch): add tab index for acceblity with tab key on switch
    
    Add tab index for acceblity with tab key on switch
    
    * test(switch): update snapshot tests for teb index
    
    Update snapshot tests for tab index
    
    * feat(switch): add onChange function and keypress enter listener
    
    add onChange function and keypress enter listener
    
    * refactor(switch): add enter key press listener
    
    Add enter key press listener
    
    * feat(switch): change eventlistener with onKeyDown prop
    
    Change eventlistener with onKeyDown prop
    
    * feat(switch): add onChange prop to switch component
    
    add onChange prop to switch component
    
    * test(switch): update switch component screenshot tests
    
    update switch component screenshot tests

--- packages/switch/src/Switch.tsx
+++ packages/switch/src/Switch.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cn from 'classnames';
 
 import { SwitchProps } from './Switch.types';
@@ -8,17 +8,38 @@ import styles from './styles/default.module.css';
 export const Switch = React.forwardRef<HTMLInputElement, SwitchProps>(
   ({ checked, onChange, disabled = false, className }, ref) => {
     const classNames = cn(styles.container, className);
+    const [isChecked, setChecked] = useState(checked);
+
+    useEffect(() => {
+      setChecked(checked);
+    }, [checked]);
+
+    const handleSwitch = () => {
+      setChecked(!isChecked);
+      onChange();
+    };
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e?.key === 'Enter') {
+        handleSwitch();
+        onChange();
+      }
+    };
 
     return (
       <label className={classNames}>
         <input
-          checked={checked}
+          checked={isChecked}
           disabled={disabled}
-          onChange={onChange}
+          onChange={() => handleSwitch()}
           ref={ref}
           type="checkbox"
         />
-        <span className={styles.switch} />
+        <span
+          className={styles.switch}
+          tabIndex={0}
+          onKeyDown={handleKeyDown}
+        />
       </label>
     );
   },
