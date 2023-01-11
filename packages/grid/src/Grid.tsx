6569e1c6178c23abbac5c6e5b8b67fe06f8af3a3
Mert Ciflikli <uzeyirmertciflikli@gmail.com>
2023-01-11 18:04:26 +0100

    refactor(grid): use const assertion for Grid

--- packages/grid/src/Grid.tsx
+++ packages/grid/src/Grid.tsx
@@ -1,23 +1,13 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { FC } from 'react';
-
 import Col from './components/Col';
-import { ColProps } from './components/Col.types';
 import Container from './components/Container';
-import { ContainerProps } from './components/Container.types';
 import Row from './components/Row';
-import { RowProps } from './components/Row.types';
 
-const Grid: {
-  Row: FC<RowProps>;
-  Col: FC<ColProps>;
-  Container: FC<ContainerProps>;
-  displayName: string;
-} = {
+const Grid = {
   Row,
   Col,
   Container,
   displayName: 'Grid',
-};
+} as const;
 
 export default Grid;
