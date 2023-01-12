635010e331969bb7693fa3281234dcaec6cba74f
Esra Altintas <esra.altintas@heycargroup.com>
2023-01-12 14:58:37 +0100

    feat(icon): change the icon name as double chevron
    change the icon name as double chevron

--- packages/icons/src/paths/DoubleChevronLeft.tsx
+++ packages/icons/src/paths/DoubleChevronLeft.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 import createSvgIcon from '../utils/createSvgIcon';
 
-export const ChevronsLeft = createSvgIcon(
+export const DoubleChevronLeft = createSvgIcon(
   <React.Fragment>
     <path
-      clip-rule="evenodd"
+      clipRule="evenodd"
       d="M6.41427 12.0711L12.7782 5.70718L11.364 4.29297L3.58584 12.0711L11.364 19.8493L12.7782 18.4351L6.41427 12.0711Z"
       fill="#303030"
-      fill-rule="evenodd"
+      fillRule="evenodd"
     />
     <path
-      clip-rule="evenodd"
+      clipRule="evenodd"
       d="M12.4143 12.0711L18.7782 5.70718L17.364 4.29297L9.58584 12.0711L17.364 19.8493L18.7782 18.4351L12.4143 12.0711Z"
       fill="#303030"
-      fill-rule="evenodd"
+      fillRule="evenodd"
     />
   </React.Fragment>,
-  'ChevronsLeft',
+  'DoubleChevronLeft',
 );
--- packages/icons/src/paths/DoubleChevronRight.tsx
+++ packages/icons/src/paths/DoubleChevronRight.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 
 import createSvgIcon from '../utils/createSvgIcon';
 
-export const ChevronsRight = createSvgIcon(
+export const DoubleChevronRight = createSvgIcon(
   <React.Fragment>
     <path
-      clip-rule="evenodd"
+      clipRule="evenodd"
       d="M17.5857 11.9289L11.2218 18.2928L12.636 19.707L20.4142 11.9289L12.636 4.15068L11.2218 5.5649L17.5857 11.9289Z"
       fill="#303030"
-      fill-rule="evenodd"
+      fillRule="evenodd"
     />
     <path
-      clip-rule="evenodd"
+      clipRule="evenodd"
       d="M11.5857 11.9289L5.22177 18.2928L6.63599 19.707L14.4142 11.9289L6.63599 4.15068L5.22177 5.5649L11.5857 11.9289Z"
       fill="#303030"
-      fill-rule="evenodd"
+      fillRule="evenodd"
     />
   </React.Fragment>,
-  'ChevronsRight',
+  'DoubleChevronRight',
 );
