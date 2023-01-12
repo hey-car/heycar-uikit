3a89b89fdf8a694db1df551e5f060dc9605c0bf8
Esra Altintas <esra.altintas@heycargroup.com>
2023-01-12 15:09:51 +0100

    feat(icon): remove the default color on new double chevron icons
    remove the default color on new double chevron icons

--- packages/icons/src/paths/DoubleChevronLeft.tsx
+++ packages/icons/src/paths/DoubleChevronLeft.tsx
@@ -7,13 +7,11 @@ export const DoubleChevronLeft = createSvgIcon(
     <path
       clipRule="evenodd"
       d="M6.41427 12.0711L12.7782 5.70718L11.364 4.29297L3.58584 12.0711L11.364 19.8493L12.7782 18.4351L6.41427 12.0711Z"
-      fill="#303030"
       fillRule="evenodd"
     />
     <path
       clipRule="evenodd"
       d="M12.4143 12.0711L18.7782 5.70718L17.364 4.29297L9.58584 12.0711L17.364 19.8493L18.7782 18.4351L12.4143 12.0711Z"
-      fill="#303030"
       fillRule="evenodd"
     />
   </React.Fragment>,
