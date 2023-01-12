d4422b1c05c8a25d00d7907b469c713eb5e1e646
Afaq Ahmed Khan <106668910+hcafaq@users.noreply.github.com>
2023-01-12 21:11:08 +0500

    [HEYUI-212] feat/implement dropdown (#121)
    * Add dropdown component
    
    * feat(dropdown): added dropdown component
    
    * basic dropdown implemetation
    
    * docs(dropdown): added docs
    
    * docs(dropdown): added design link
    
    * test(dropdown): added tests for dropdown
    
    * test(dropdown): adds screenshot tests
    
    * fix(dropdown): adds a null check for onChange
    
    * fix(dropdown): resolve prettier error
    
    * fix(dropdown): update screenshots to pass tests
    
    * test(dropdown): added tests for dropdown
    
    * test(dropdown): removed unnecessary comments
    
    * test(dropdown): update tests
    
    * test(dropdown): update tests
    
    * test(typography): update typography screenshot tests
    
    update typography screenshot tests
    
    * style(dropdown): content alignment with left icon
    
    * docs(dropdown): updated props for canvas
    
    * style(dropdown): addressed design comments
    
    * test(dropdown): update sh
    
    * fix(dropdown): resolve lintin issue
    
    * fix(dropdown): alignmenmt fix
    
    * style(dropdown): update color var
    
    * fix(dropdown): design update
    
    * fix(dropdown): added padding
    
    * fix(dropdown): design update
    
    * fix(dropdown): formatting update
    
    * ci(dropdown): re-running build to get it deployed again
    
    * fix(dropdown): update design height
    
    * fix(dropdown): color fix
    
    Co-authored-by: Esra Altintas <esra.altintas@heycargroup.com>

---
+++ packages/dropdown/src/__tests__/Dropdown.snapshot.test.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Dropdown from '../Dropdown';
+
+describe('Dropdown Snapshots tests', () => {
+  it('should match callback methods', () => {
+    expect(
+      render(
+        <Dropdown
+          onChange={jest.fn()}
+          options={[
+            {
+              value: 'pomelo',
+              label: 'Pomelo',
+            },
+            {
+              value: 'apple',
+              label: 'Apple',
+            },
+            {
+              value: 'mango',
+              label: 'Mango',
+            },
+          ]}
+          value={{
+            value: 'mango',
+            label: 'Mango',
+          }}
+        />,
+      ),
+    ).toMatchSnapshot();
+  });
+
+  it('should match disabled state', () => {
+    expect(
+      <Dropdown
+        disabled={true}
+        onChange={() => {}}
+        options={[
+          {
+            value: 'pomelo',
+            label: 'Pomelo',
+          },
+          {
+            value: 'apple',
+            label: 'Apple',
+          },
+          {
+            value: 'mango',
+            label: 'Mango',
+          },
+        ]}
+        value={{
+          value: 'mango',
+          label: 'Mango',
+        }}
+      />,
+    ).toMatchSnapshot();
+  });
+});
---
+++ packages/dropdown/src/__tests__/Dropdown.test.tsx
@@ -0,0 +1,243 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+
+import Dropdown from '../Dropdown';
+
+describe('Dropdown', () => {
+  describe('Prop tests', () => {
+    it('should set value', () => {
+      const { container } = render(
+        <Dropdown
+          onChange={() => { }}
+          options={[
+            {
+              value: 'pomelo',
+              label: 'Pomelo',
+            },
+            {
+              value: 'apple',
+              label: 'Apple',
+            },
+            {
+              value: 'mango',
+              label: 'Mango',
+            },
+          ]}
+          value={{
+            value: 'mango',
+            label: 'Mango',
+          }}
+        />,
+      );
+
+      expect(container.querySelector('span')).toHaveTextContent('Mango');
+    });
+
+
+    it('should set options', () => {
+      const { container } = render(
+        <Dropdown
+          onChange={() => { }}
+          options={[
+            {
+              value: 'pomelo',
+              label: 'Pomelo',
+            },
+            {
+              value: 'apple',
+              label: 'Apple',
+            },
+            {
+              value: 'mango',
+              label: 'Mango',
+            },
+          ]}
+          value={{
+            value: 'mango',
+            label: 'Mango',
+          }}
+        />,
+      );
+
+      expect(container.getElementsByTagName('li')).toHaveLength(3);
+    });
+  });
+
+  describe('State tests', () => {
+    it('should set state disabled', () => {
+      const { container } = render(
+        <Dropdown
+          disabled={true}
+          onChange={() => { }}
+          options={[
+            {
+              value: 'pomelo',
+              label: 'Pomelo',
+            },
+            {
+              value: 'apple',
+              label: 'Apple',
+            },
+            {
+              value: 'mango',
+              label: 'Mango',
+            },
+          ]}
+          value={{
+            value: 'mango',
+            label: 'Mango',
+          }}
+        />,
+      );
+
+      expect(container.querySelector('span')).toHaveClass('disabled');
+    });
+  });
+
+  describe('Callbacks tests', () => {
+    it('should call `onChange` prop', () => {
+      const cb = jest.fn();
+      const dataTestId = 'testId';
+      const { getByTestId } = render(
+        <Dropdown
+          dataTestId={dataTestId}
+          onChange={cb}
+          options={[
+            {
+              value: 'pomelo',
+              label: 'Pomelo',
+            },
+            {
+              value: 'apple',
+              label: 'Apple',
+            },
+            {
+              value: 'mango',
+              label: 'Mango',
+            },
+          ]}
+          value={{
+            value: 'mango',
+            label: 'Mango',
+          }}
+        />,
+      );
+
+      const ul = getByTestId(dataTestId) as HTMLUListElement;
+
+      if(ul.firstChild) fireEvent.click(ul.firstChild);
+
+      expect(cb).toBeCalledTimes(1);
+    });
+
+    it('should call `onClick` prop', () => {
+      const cb = jest.fn();
+      const dataTestId = 'testId';
+      const { container } = render(
+        <Dropdown
+          dataTestId={dataTestId}
+          onClick={cb}
+          options={[
+            {
+              value: 'pomelo',
+              label: 'Pomelo',
+            },
+            {
+              value: 'apple',
+              label: 'Apple',
+            },
+            {
+              value: 'mango',
+              label: 'Mango',
+            },
+          ]}
+          value={{
+            value: 'mango',
+            label: 'Mango',
+          }}
+        />,
+      );
+
+      const ul = container as HTMLUListElement;
+
+      if(ul.firstChild) fireEvent.click(ul.firstChild);
+
+      expect(cb).toBeCalledTimes(1);
+    });
+
+    it('should call `onBlur` prop', () => {
+      const cb = jest.fn();
+      const cb1 = jest.fn();
+      const dataTestId = 'testId';
+      const { container } = render(
+        <Dropdown
+          dataTestId={dataTestId}
+          onBlur={cb}
+          onClick={cb1}
+          options={[
+            {
+              value: 'pomelo',
+              label: 'Pomelo',
+            },
+            {
+              value: 'apple',
+              label: 'Apple',
+            },
+            {
+              value: 'mango',
+              label: 'Mango',
+            },
+          ]}
+          value={{
+            value: 'mango',
+            label: 'Mango',
+          }}
+        />,
+      );
+
+      const ul = container as HTMLUListElement;
+
+      if (ul.firstChild) fireEvent.click(ul.firstChild);
+      if(ul.firstChild) fireEvent.blur(ul.firstChild);
+
+      expect(cb).toBeCalledTimes(1);
+    });
+
+
+    it('should set `options` through prop', () => {
+
+      const dataTestId = 'testId';
+      const options = [
+        {
+          value: 'pomelo',
+          label: 'Pomelo',
+        },
+        {
+          value: 'apple',
+          label: 'Apple',
+        },
+        {
+          value: 'mango',
+          label: 'Mango',
+        },
+      ];
+      const { getByTestId } = render(
+        <Dropdown
+          dataTestId={dataTestId}
+          options={[...options]}
+          value={{
+            value: 'mango',
+            label: 'Mango',
+          }}
+        />,
+      );
+
+      const ul = getByTestId(dataTestId) as HTMLUListElement;
+
+      console.log('ul', ul);
+
+      expect(ul.childElementCount).toBe(options.length);
+    });
+  });
+});
