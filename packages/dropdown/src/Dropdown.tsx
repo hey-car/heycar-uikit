728b0d25b7b0ac7db0fb4d07aa142b18d24ce0e9
hcafaq <afaq.khan@heycargroup.com>
2023-02-03 09:59:44 +0100

    feat(dropdown): added select element to dropdown

--- packages/dropdown/src/Dropdown.tsx
+++ packages/dropdown/src/Dropdown.tsx
@@ -6,104 +6,107 @@ import { DropdownProps, SelectOptions } from './Dropdown.types';
 
 import styles from './styles/default.module.css';
 
-function Dropdown({
-  Component = 'div',
-  value,
-  onChange,
-  options,
-  disabled,
-  dataTestId,
-  onBlur,
-  onClick,
-  fullWidth,
-  ...restProps
-}: DropdownProps) {
-  const [stateValue, setStateValue] = useState<SelectOptions | undefined>(
-    value,
-  );
+const Dropdown = React.forwardRef<HTMLSelectElement, DropdownProps>(
+  (
+    {
+      Component = 'div',
+      value,
+      onChange,
+      options,
+      disabled,
+      dataTestId,
+      onBlur,
+      onClick,
+      fullWidth,
+      ...restProps
+    },
+    ref,
+  ) => {
+    const [stateValue, setStateValue] = useState<SelectOptions | undefined>(
+      value,
+    );
 
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const selectOption = (option: SelectOptions) => {
-    if (onChange) {
-      if (option !== stateValue) onChange(option);
-    }
-    setStateValue(option);
-  };
+    const selectOption = (option: SelectOptions) => {
+      if (onChange) {
+        if (option !== stateValue) onChange(option);
+      }
+      setStateValue(option);
+    };
 
-  const isOptionSelection = (option: SelectOptions) => {
-    return option?.value === stateValue?.value;
-  };
+    const isOptionSelection = (option: SelectOptions) => {
+      return option?.value === stateValue?.value;
+    };
 
-  const onClickHandler = () => {
-    if (onClick) onClick();
-    if (!disabled) setIsOpen(!isOpen);
-  };
-  const onBlurHandler = () => {
-    if (onBlur) onBlur();
-    setIsOpen(false);
-  };
+    const onClickHandler = () => {
+      if (onClick) onClick();
+      if (!disabled) setIsOpen(!isOpen);
+    };
+    const onBlurHandler = () => {
+      if (onBlur) onBlur();
+      setIsOpen(false);
+    };
 
-  const classNames = cn(
-    styles.container,
-    disabled && styles.disabled,
-    fullWidth && styles.fullWidth,
-  );
+    const classNames = cn(
+      styles.container,
+      disabled && styles.disabled,
+      fullWidth && styles.fullWidth,
+    );
 
-  const arrowClassNames = cn(
-    disabled && styles.caret_disabled,
-    isOpen && styles.caret_up,
-    !isOpen && styles.caret_down,
-  );
+    const arrowClassNames = cn(
+      disabled && styles.caret_disabled,
+      isOpen && styles.caret_up,
+      !isOpen && styles.caret_down,
+    );
 
-  const valueClassNames = cn(
-    disabled && 'disabled',
-    styles.value,
-  );
+    const valueClassNames = cn(disabled && 'disabled', styles.value);
 
-  return (
-    <Component
-      className={classNames}
-      onBlur={onBlurHandler}
-      onClick={onClickHandler}
-      tabIndex={0}
-      {...restProps}
-    >
-      <span className={valueClassNames}>
-        {stateValue?.label}
-      </span>
-      <div className={arrowClassNames}></div>
-      <ul
-        className={`${styles.options} ${isOpen ? styles.show : ''}`}
-        data-test-id={dataTestId}
+    return (
+      <Component
+        className={classNames}
+        onBlur={onBlurHandler}
+        onClick={onClickHandler}
+        ref={ref}
+        tabIndex={0}
+        {...restProps}
       >
-        {options.map(option => (
-          <li
-            className={`${styles.option} ${isOptionSelection(option) ? styles.selected : ''
-              }`}
-            key={option.value}
-            onClick={e => {
-              e.stopPropagation();
-              selectOption(option);
-              setIsOpen(false);
-            }}
-          >
-            <div className={styles.contentContainer}>
-              {option.leftContent && (
-                <span className={styles.leftContent}>{option.leftContent}</span>
-              )}
-              <span className={styles.optionContent}>{option.label}</span>
-              {option.rightContent && (
-                <span className={styles.rightContent}>
-                  {option.rightContent}
-                </span>
-              )}
-            </div>
-          </li>
-        ))}
-      </ul>
-    </Component>
-  );
-}
+        <span className={valueClassNames}>{stateValue?.label}</span>
+        <div className={arrowClassNames}></div>
+        <ul
+          className={`${styles.options} ${isOpen ? styles.show : ''}`}
+          data-test-id={dataTestId}
+        >
+          {options.map(option => (
+            <li
+              className={`${styles.option} ${isOptionSelection(option) ? styles.selected : ''
+                }`}
+              key={option.value}
+              onClick={e => {
+                e.stopPropagation();
+                selectOption(option);
+                setIsOpen(false);
+              }}
+            >
+              <div className={styles.contentContainer}>
+                {option.leftContent && (
+                  <span className={styles.leftContent}>
+                    {option.leftContent}
+                  </span>
+                )}
+                <span className={styles.optionContent}>{option.label}</span>
+                {option.rightContent && (
+                  <span className={styles.rightContent}>
+                    {option.rightContent}
+                  </span>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Component>
+    );
+  },
+);
 
 export default Dropdown;
