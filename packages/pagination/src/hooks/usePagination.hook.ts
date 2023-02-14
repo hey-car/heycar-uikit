81da0dd812f46cfdba84646084064b741e474bb5
Lucas <lucas.maffazioli@gmail.com>
2023-02-14 17:04:55 +0100

    refactor(pagination): typography error; better paginationItems

--- packages/pagination/src/hooks/usePagination.hook.ts
+++ packages/pagination/src/hooks/usePagination.hook.ts
@@ -49,140 +49,123 @@ const usePagination = ({
 
   const siblingsToRender = getSiblingsToRender(totalPages, currentPage);
 
-  const items: PaginationItemProps[] = isDesktop
-    ? [
-        // Previous button
-        {
-          itemType: PaginationItemType.Previous,
-          page: currentPage - 1,
-          isDisabled: currentPage === 1,
-          onClick: parseOnClick({
-            onClick,
-            pageNumber: currentPage - 1,
-            isDisabled: currentPage === 1,
-          }),
-          'aria-label': locale.prevPage,
-        },
-        // First page
-        {
-          itemType: PaginationItemType.PageNumber,
-          page: 1,
-          isCurrentPage: currentPage === 1,
-          onClick: parseOnClick({ onClick, pageNumber: 1, isDisabled: false }),
-          'aria-label': getLabelForPageNumbers(1, currentPage === 1, locale),
-        },
-        // Ellipsis
-        ...shouldReturnObjInArray(shouldShowPreDots, {
-          itemType: PaginationItemType.Ellipsis,
-        }),
-        // Siblings
-        ...siblingsToRender.map(page => ({
-          itemType: PaginationItemType.PageNumber,
-          page: page,
-          isCurrentPage: currentPage === page,
-          onClick: parseOnClick({
-            onClick,
-            pageNumber: page,
-            isDisabled: false,
-          }),
-          'aria-label': getLabelForPageNumbers(
-            page,
-            currentPage === page,
-            locale,
-          ),
-        })),
-        // Ellipsis
-        ...shouldReturnObjInArray(shouldShowPostDots, {
-          itemType: PaginationItemType.Ellipsis,
-        }),
-        // Last Page
-        ...shouldReturnObjInArray(totalPages > 1, {
-          itemType: PaginationItemType.PageNumber,
-          page: totalPages,
-          isCurrentPage: currentPage === totalPages,
-          onClick: parseOnClick({
-            onClick,
-            pageNumber: totalPages,
-            isDisabled: false,
-          }),
-          'aria-label': getLabelForPageNumbers(
-            totalPages,
-            currentPage === totalPages,
-            locale,
-          ),
-        }),
-        // Next button
-        {
-          itemType: PaginationItemType.Next,
-          page: currentPage + 1,
-          isDisabled: currentPage === totalPages,
-          onClick: parseOnClick({
-            onClick,
-            pageNumber: currentPage + 1,
-            isDisabled: currentPage === totalPages,
-          }),
-          'aria-label': locale.nextPage,
-        },
-      ]
-    : [
-        // Previous button
-        {
-          itemType: PaginationItemType.Previous,
-          page: currentPage - 1,
-          isDisabled: currentPage === 1,
-          onClick: parseOnClick({
-            onClick,
-            pageNumber: currentPage - 1,
-            isDisabled: currentPage === 1,
-          }),
-          'aria-label': locale.prevPage,
-        },
-        // Current page
-        ...shouldReturnObjInArray(
-          currentPage !== totalPages || totalPages === 1,
+  const items: PaginationItemProps[] = [
+    // Previous button
+    {
+      itemType: PaginationItemType.Previous,
+      page: currentPage - 1,
+      isDisabled: currentPage === 1,
+      onClick: parseOnClick({
+        onClick,
+        pageNumber: currentPage - 1,
+        isDisabled: currentPage === 1,
+      }),
+      'aria-label': locale.prevPage,
+    },
+    ...(isDesktop
+      ? [
+          // First page
           {
             itemType: PaginationItemType.PageNumber,
-            page: currentPage,
+            page: 1,
+            isCurrentPage: currentPage === 1,
             onClick: parseOnClick({
               onClick,
-              pageNumber: currentPage,
+              pageNumber: 1,
               isDisabled: false,
             }),
-            'aria-label': getLabelForPageNumbers(currentPage, true, locale),
+            'aria-label': getLabelForPageNumbers(1, currentPage === 1, locale),
           },
-        ),
-        // Slash
-        ...shouldReturnObjInArray(currentPage !== totalPages, {
-          itemType: PaginationItemType.Slash,
-        }),
-        // Last Page
-        ...shouldReturnObjInArray(totalPages > 1, {
-          itemType: PaginationItemType.PageNumber,
-          page: totalPages,
-          onClick: parseOnClick({
-            onClick,
-            pageNumber: totalPages,
-            isDisabled: false,
+          // Ellipsis
+          ...shouldReturnObjInArray(shouldShowPreDots, {
+            itemType: PaginationItemType.Ellipsis,
           }),
-          'aria-label': getLabelForPageNumbers(
-            totalPages,
-            currentPage === totalPages,
-            locale,
+          // Siblings
+          ...siblingsToRender.map(page => ({
+            itemType: PaginationItemType.PageNumber,
+            page: page,
+            isCurrentPage: currentPage === page,
+            onClick: parseOnClick({
+              onClick,
+              pageNumber: page,
+              isDisabled: false,
+            }),
+            'aria-label': getLabelForPageNumbers(
+              page,
+              currentPage === page,
+              locale,
+            ),
+          })),
+          // Ellipsis
+          ...shouldReturnObjInArray(shouldShowPostDots, {
+            itemType: PaginationItemType.Ellipsis,
+          }),
+          // Last Page
+          ...shouldReturnObjInArray(totalPages > 1, {
+            itemType: PaginationItemType.PageNumber,
+            page: totalPages,
+            isCurrentPage: currentPage === totalPages,
+            onClick: parseOnClick({
+              onClick,
+              pageNumber: totalPages,
+              isDisabled: false,
+            }),
+            'aria-label': getLabelForPageNumbers(
+              totalPages,
+              currentPage === totalPages,
+              locale,
+            ),
+          }),
+        ]
+      : [
+          // Current page
+          ...shouldReturnObjInArray(
+            currentPage !== totalPages || totalPages === 1,
+            {
+              itemType: PaginationItemType.PageNumber,
+              page: currentPage,
+              onClick: parseOnClick({
+                onClick,
+                pageNumber: currentPage,
+                isDisabled: false,
+              }),
+              'aria-label': getLabelForPageNumbers(currentPage, true, locale),
+            },
           ),
-        }),
-        // Next button
-        {
-          itemType: PaginationItemType.Next,
-          page: currentPage + 1,
-          isDisabled: currentPage === totalPages,
-          onClick: parseOnClick({
-            onClick,
-            pageNumber: currentPage + 1,
-            isDisabled: currentPage === totalPages,
+          // Slash
+          ...shouldReturnObjInArray(currentPage !== totalPages, {
+            itemType: PaginationItemType.Slash,
           }),
-          'aria-label': locale.nextPage,
-        },
-      ];
+          // Last Page
+          ...shouldReturnObjInArray(totalPages > 1, {
+            itemType: PaginationItemType.PageNumber,
+            page: totalPages,
+            onClick: parseOnClick({
+              onClick,
+              pageNumber: totalPages,
+              isDisabled: false,
+            }),
+            'aria-label': getLabelForPageNumbers(
+              totalPages,
+              currentPage === totalPages,
+              locale,
+            ),
+          }),
+        ]),
+
+    // Next button
+    {
+      itemType: PaginationItemType.Next,
+      page: currentPage + 1,
+      isDisabled: currentPage === totalPages,
+      onClick: parseOnClick({
+        onClick,
+        pageNumber: currentPage + 1,
+        isDisabled: currentPage === totalPages,
+      }),
+      'aria-label': locale.nextPage,
+    },
+  ];
 
   return { items };
 };
