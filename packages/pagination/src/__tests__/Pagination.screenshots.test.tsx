d3eb288996d38445e9a0c7a91a29bafffea5d6ec
Nathan Jamal <nathanjamal@ymail.com>
2023-03-21 16:05:36 +0000

    update screenshot tests and screenshots

--- packages/pagination/src/__tests__/Pagination.screenshots.test.tsx
+++ packages/pagination/src/__tests__/Pagination.screenshots.test.tsx
@@ -12,60 +12,159 @@ const clip = { x: 0, y: 0, width: 750, height: 550 };
 const desktopWidthSize = 1280;
 const mobileWidthSize = 320;
 
-const viewPort = {
-  width: desktopWidthSize,
-  height: 768,
+const runTest = async (pageUrl: string, isMobile?: boolean) => {
+  const { browser, context, page } = await openBrowserPage(pageUrl, undefined, {
+    width: isMobile ? mobileWidthSize : desktopWidthSize,
+    height: 768,
+  });
+
+  try {
+    await matchHtml({
+      page,
+      expect,
+      screenshotOpts: { clip },
+    });
+  } catch (error: unknown) {
+    console.error(error);
+  } finally {
+    await closeBrowser({ browser, context, page });
+  }
 };
 
 describe('Pagination screenshots tests', () => {
-  test('Screen size = desktop', async () => {
-    const pageUrl = createStorybookUrl({
-      packageName,
+  describe('Screen size = desktop', () => {
+    test('currentPage=1 totalPages=1', async () => {
+      const pageUrl = createStorybookUrl({
+        packageName,
+        knobs: {
+          currentPage: 1,
+          totalPages: 1,
+        },
+      });
+
+      await runTest(pageUrl);
     });
 
-    const { browser, context, page } = await openBrowserPage(
-      pageUrl,
-      undefined,
-      viewPort,
-    );
-
-    try {
-      await matchHtml({
-        page,
-        expect,
-        screenshotOpts: { clip },
+    test('currentPage=1 totalPages=2', async () => {
+      const pageUrl = createStorybookUrl({
+        packageName,
+        knobs: {
+          currentPage: 1,
+          totalPages: 2,
+        },
       });
-    } catch (error: unknown) {
-      console.error(error);
-    } finally {
-      await closeBrowser({ browser, context, page });
-    }
+
+      await runTest(pageUrl);
+    });
+
+    test('currentPage=1 totalPages=3', async () => {
+      const pageUrl = createStorybookUrl({
+        packageName,
+        knobs: {
+          currentPage: 1,
+          totalPages: 3,
+        },
+      });
+
+      await runTest(pageUrl);
+    });
+
+    test('currentPage=1 totalPages=4', async () => {
+      const pageUrl = createStorybookUrl({
+        packageName,
+        knobs: {
+          currentPage: 1,
+          totalPages: 4,
+        },
+      });
+
+      await runTest(pageUrl);
+    });
+
+    test('currentPage=1 totalPages=5', async () => {
+      const pageUrl = createStorybookUrl({
+        packageName,
+        knobs: {
+          currentPage: 1,
+          totalPages: 5,
+        },
+      });
+
+      await runTest(pageUrl);
+    });
+
+    test('currentPage=2 totalPages=5', async () => {
+      const pageUrl = createStorybookUrl({
+        packageName,
+        knobs: {
+          currentPage: 2,
+          totalPages: 5,
+        },
+      });
+
+      await runTest(pageUrl);
+    });
+
+    test('currentPage=5 totalPages=5', async () => {
+      const pageUrl = createStorybookUrl({
+        packageName,
+        knobs: {
+          currentPage: 5,
+          totalPages: 5,
+        },
+      });
+
+      await runTest(pageUrl);
+    });
   });
 
-  test('Screen size = mobile', async () => {
-    const pageUrl = createStorybookUrl({
-      packageName,
+  describe('Screen size = mobile', () => {
+    test('currentPage=1 totalPages=1', async () => {
+      const pageUrl = createStorybookUrl({
+        packageName,
+        knobs: {
+          currentPage: 1,
+          totalPages: 1,
+        },
+      });
+
+      await runTest(pageUrl, true);
     });
 
-    const { browser, context, page } = await openBrowserPage(
-      pageUrl,
-      undefined,
-      {
-        ...viewPort,
-        width: mobileWidthSize,
-      },
-    );
-
-    try {
-      await matchHtml({
-        page,
-        expect,
-        screenshotOpts: { clip },
+    test('currentPage=1 totalPages=2', async () => {
+      const pageUrl = createStorybookUrl({
+        packageName,
+        knobs: {
+          currentPage: 1,
+          totalPages: 2,
+        },
       });
-    } catch (error: unknown) {
-      console.error(error);
-    } finally {
-      await closeBrowser({ browser, context, page });
-    }
+
+      await runTest(pageUrl, true);
+    });
+
+    test('currentPage=2 totalPages=5', async () => {
+      const pageUrl = createStorybookUrl({
+        packageName,
+        knobs: {
+          currentPage: 2,
+          totalPages: 5,
+        },
+      });
+
+      await runTest(pageUrl, true);
+    });
+
+    test('currentPage=5 totalPages=5', async () => {
+      const pageUrl = createStorybookUrl({
+        packageName,
+        knobs: {
+          currentPage: 5,
+          totalPages: 5,
+        },
+      });
+
+      await runTest(pageUrl, true);
+    });
   });
 });
