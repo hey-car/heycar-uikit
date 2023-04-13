6eb9bc1c635af25d6d1e4c75f2b3857ae07ebe79
Nathan Jamal <nathanjamal@ymail.com>
2023-04-13 10:46:39 +0100

    update screenshot test

--- packages/review-rating/src/__tests__/ReviewRating.screenshots.test.tsx
+++ packages/review-rating/src/__tests__/ReviewRating.screenshots.test.tsx
@@ -9,7 +9,7 @@ const scores = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const clip = { x: 0, y: 0, width: 500, height: 200 };
 // the svg "donut" renders slightly different at its edges on different devices. the blur gets around that
-const blur = 1.5;
+const blur = 2;
 
 const screenshotTesting = setupScreenshotTesting({
   it,
