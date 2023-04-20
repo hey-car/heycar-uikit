b41935c9f0fe25e614b20a0f391a02d1057eedbe
Esra Altintas <esra.altintas@heycargroup.com>
2023-04-20 19:43:39 +0200

    Limit 20ch per one breadcrumb

--- packages/breadcrumbs/src/__tests__/Breadcrumbs.snapshot.test.tsx
+++ packages/breadcrumbs/src/__tests__/Breadcrumbs.snapshot.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Accordion from '../Accordion';
+import Breadcrumbs from '../Breadcrumbs';
 
 const accordionTitle = 'title';
 const accordionBodyText =
