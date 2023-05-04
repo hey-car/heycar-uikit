7991a6df262188e9938acc81ad342bdf044d3e88
nathan jamal <nathanjamal@ymail.com>
2023-05-04 18:53:05 +0100

    feat(header): add Header component (#201)
    * feat(test): test component
    
    added test component to see if it will build
    
    new-pkg-test
    
    * renamed header
    
    * remove docs
    
    * added header src files
    
    * added docs
    
    * added tests
    
    * update some ts
    
    * update styles
    
    * update screenshots
    
    * disign review
    
    * update readme and docs

---
+++ packages/header/src/Header.tsx
@@ -0,0 +1,291 @@
+import React, { useState } from 'react';
+
+import Grid from '@heycar-uikit/grid';
+import {
+  Call,
+  Close,
+  Dealer,
+  Hamburger,
+  HeartDefault,
+  Search,
+} from '@heycar-uikit/icons';
+import Logo from '@heycar-uikit/logo';
+import Typography from '@heycar-uikit/typography';
+
+import LanguageList from './components/LanguageList';
+import NavigationBurgerMenu from './components/NavigationBurgerMenu';
+import NavigationDropdown from './components/NavigationDropdown';
+import { useLangList } from './hooks/useLangList';
+import {
+  getCurrentLang,
+  hasHeaderItems,
+  itemOnClick,
+} from './utils/headerItemHelpers';
+import { DEFAULT_LOCALE, MAX_FAVES_DISPLAY_NO } from './Header.constants';
+import { HeaderLinkProps, HeaderProps } from './Header.types';
+
+import styles from './styles/default.module.css';
+
+const DefaultLinkComponent = (props: HeaderLinkProps) => <a {...props} />;
+
+const Header = React.forwardRef<HTMLElement, HeaderProps>(
+  (
+    {
+      accountItemConfig,
+      auxiliaryDetails,
+      callItemConfig,
+      dataTestId,
+      favoritesItemConfig,
+      langItemConfig,
+      LinkComponent,
+      locale = DEFAULT_LOCALE,
+      logoHref,
+      navigation,
+      searchItemConfig,
+      trackingFn,
+    },
+    ref,
+  ) => {
+    // State items
+    const [isNavTrayOpen, setIsNavTrayOpen] = useState(false);
+    const [activeNavItem, setActiveNavItem] = useState<string | undefined>(
+      undefined,
+    );
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+
+    // Link component
+    const Link = (LinkComponent || DefaultLinkComponent) as (
+      props: HeaderLinkProps,
+    ) => JSX.Element;
+
+    // Header items
+    const { hasSearch, hasFaves, hasLang, hasAccount, hasCall } =
+      hasHeaderItems(
+        searchItemConfig,
+        favoritesItemConfig,
+        langItemConfig,
+        accountItemConfig,
+        callItemConfig,
+      );
+    const favesCount = favoritesItemConfig!.favoritesNumber || 0;
+
+    // Lang
+    const currentLang = hasLang
+      ? getCurrentLang(langItemConfig!.currentLang, langItemConfig!.options)
+      : undefined;
+    const LangIco = currentLang?.icon;
+    const { isLangListOpen, keyboardOpen, setIsFocused, setIsHovering } =
+      useLangList();
+
+    // Event handlers
+    const handleSearchToggle = (newState: boolean) => {
+      setIsSearchOpen(newState);
+      if (searchItemConfig?.onClick) searchItemConfig.onClick(newState);
+    };
+
+    return (
+      <header className={styles.header} data-test-id={dataTestId} ref={ref}>
+        <Grid.Container className={styles.headerInner}>
+          <Grid.Row gutter={{ mobile: 8, tablet: 12, desktop: 24 }}>
+            <Grid.Col className={styles.colLeft}>
+              <Link
+                aria-label={locale.logoLabel}
+                className={styles.logo}
+                href={logoHref}
+                onClick={() => itemOnClick('Logo', trackingFn)}
+              >
+                <Logo />
+              </Link>
+              {hasSearch && (
+                <div
+                  className={`${styles.searchWrapper} ${
+                    isSearchOpen ? styles.active : ''
+                  }`}
+                >
+                  <button
+                    aria-label={locale.closeSearchLabel}
+                    className={styles.closeSearch}
+                    onClick={() => handleSearchToggle(false)}
+                  >
+                    <Close />
+                  </button>
+                  {searchItemConfig!.Component}
+                </div>
+              )}
+            </Grid.Col>
+            <Grid.Col className={styles.colRight}>
+              {/*
+               *** Mobile Search Button
+               */}
+              {hasSearch && (
+                <button
+                  aria-label={searchItemConfig!.label}
+                  className={`${styles.notHorizontalNav} ${styles.item}`}
+                  onClick={() => handleSearchToggle(true)}
+                >
+                  <Search />
+                </button>
+              )}
+              {/*
+               *** Favorite Button
+               */}
+              {hasFaves && (
+                <Link
+                  aria-label={favoritesItemConfig!.label}
+                  className={`${styles.item} ${styles.faves}`}
+                  href={favoritesItemConfig!.href}
+                  onClick={() =>
+                    itemOnClick(
+                      favoritesItemConfig!.label,
+                      trackingFn,
+                      favoritesItemConfig!.onClick,
+                    )
+                  }
+                >
+                  <HeartDefault />
+                  <span
+                    aria-label={locale.favoritesCountLabel}
+                    className={`${styles.counter} ${
+                      favesCount > 0 ? styles.active : ''
+                    }`}
+                  >
+                    {favesCount > MAX_FAVES_DISPLAY_NO
+                      ? MAX_FAVES_DISPLAY_NO
+                      : favesCount}
+                  </span>
+                  <Typography variant="subheading3">
+                    {favoritesItemConfig!.label}
+                  </Typography>
+                </Link>
+              )}
+              {/*
+               *** Lang Select
+               */}
+              {hasLang && currentLang && (
+                <div className={styles.langWrapper}>
+                  <button
+                    aria-haspopup="menu"
+                    aria-label={`${locale.langListHeading} - ${locale.spaceBarNotification}`}
+                    className={`${styles.horizontalNavOnly} ${styles.item} ${
+                      isLangListOpen ? styles.focused : ''
+                    }`}
+                    onBlur={() => setIsFocused(false)}
+                    onFocus={() => setIsFocused(true)}
+                    onKeyDown={e => keyboardOpen(e)}
+                    onMouseOut={() => setIsHovering(false)}
+                    onMouseOver={() => setIsHovering(true)}
+                  >
+                    {LangIco}
+                    <Typography variant="subheading3">
+                      {currentLang.shortName}
+                    </Typography>
+                  </button>
+                  {isLangListOpen && (
+                    <LanguageList
+                      dataTestId="header-language-list"
+                      heading={locale.langListHeading}
+                      onFocusEvents={setIsFocused}
+                      onHoverEvents={setIsHovering}
+                      options={langItemConfig!.options}
+                      trackingFn={trackingFn}
+                    />
+                  )}
+                </div>
+              )}
+              {/*
+               *** Account Button
+               */}
+              {hasAccount && (
+                <button
+                  aria-label={accountItemConfig!.label}
+                  className={`${styles.horizontalNavOnly} ${styles.item}`}
+                  onClick={() =>
+                    itemOnClick(
+                      accountItemConfig!.label,
+                      trackingFn,
+                      accountItemConfig!.onClick,
+                    )
+                  }
+                >
+                  <Dealer />
+                  <Typography variant="subheading3">
+                    {accountItemConfig!.label}
+                  </Typography>
+                </button>
+              )}
+              {/*
+               *** Call Button
+               */}
+              {hasCall && (
+                <Link
+                  aria-label={callItemConfig!.label}
+                  className={`${styles.horizontalNavOnly} ${styles.item} ${styles.asideItem}`}
+                  href={callItemConfig!.href}
+                  onClick={() =>
+                    itemOnClick(
+                      callItemConfig!.label,
+                      trackingFn,
+                      callItemConfig!.onClick,
+                    )
+                  }
+                >
+                  <Call />
+                  <Typography variant="subheading3">
+                    {callItemConfig!.label}
+                  </Typography>
+                </Link>
+              )}
+              {/*
+               *** Mobile menu Toggle
+               */}
+              <button
+                aria-label={DEFAULT_LOCALE.burgerMenuButtonLabel}
+                className={`${styles.notHorizontalNav} ${styles.item}`}
+                onClick={() =>
+                  itemOnClick('Toggle Menu', trackingFn, () =>
+                    setIsNavTrayOpen(!isNavTrayOpen),
+                  )
+                }
+              >
+                {isNavTrayOpen ? <Close /> : <Hamburger />}
+              </button>
+            </Grid.Col>
+          </Grid.Row>
+        </Grid.Container>
+        {/*
+         *** Navigation
+         */}
+        <div
+          className={`${styles.navWrapper} ${isNavTrayOpen ? 'navOpen' : ''}`}
+        >
+          <NavigationBurgerMenu
+            Link={Link}
+            accountItemConfig={accountItemConfig}
+            activeNavItem={activeNavItem}
+            auxiliaryDetails={auxiliaryDetails}
+            currentLang={currentLang?.langCode}
+            dataTestId={`${dataTestId}-navigation`}
+            langItemConfig={langItemConfig}
+            locale={locale}
+            navigation={navigation}
+            setActiveNavItem={setActiveNavItem}
+            trackingFn={trackingFn}
+          />
+          <NavigationDropdown
+            Link={Link}
+            activeNavItem={activeNavItem}
+            dataTestId={`${dataTestId}-navigation`}
+            locale={locale}
+            navigation={navigation}
+            setActiveNavItem={setActiveNavItem}
+            trackingFn={trackingFn}
+          />
+        </div>
+      </header>
+    );
+  },
+);
+
+Header.displayName = 'Header';
+
+export default Header;
---
+++ packages/header/src/components/Navigation.types.ts
@@ -0,0 +1,153 @@
+import {
+  HeaderLinkProps,
+  HeaderProps,
+  HeaderTrackingObj,
+  Locale,
+} from '../Header.types';
+
+interface SubNavItem {
+  /**
+   * Description text to be displayed under the label
+   */
+  desc?: string;
+  /**
+   * Href for navigation on click
+   */
+  href: string;
+  /**
+   * Text label for the link
+   */
+  label: string;
+}
+
+interface SubNavGroup {
+  /**
+   * Heading for the group
+   */
+  heading: string;
+  /**
+   * SubNavItem array
+   */
+  items: SubNavItem[];
+  /**
+   * Text label for the link
+   */
+  showAllLink?: {
+    /**
+     * Href for navigation on click
+     */
+    href?: string;
+    /**
+     * Function to run on click
+     */
+    onClick?: () => void;
+    /**
+     * Text label to override the default value in locale
+     */
+    label?: string;
+    /**
+     * Hide this item
+     */
+    hide?: boolean;
+  };
+}
+
+interface NavItem {
+  /**
+   * Text label to be displayed
+   */
+  label: string;
+  /**
+   * Href for navigation on click (only use if item has no sub nav groups)
+   */
+  href?: string;
+  /**
+   * Function to run on click
+   */
+  onClick?: () => void;
+  /**
+   * SubNavGroup array
+   */
+  subNavGroups?: SubNavGroup[];
+  /**
+   * Only show this item in the burger menu, Not on the dropdown version of the menu
+   */
+  isBurgerMenuOnly?: boolean;
+}
+
+interface NavigationProps
+  extends Pick<HeaderProps, 'accountItemConfig' | 'langItemConfig'> {
+  /**
+   * Id of the current open nav item
+   */
+  activeNavItem?: string;
+  /**
+   * Supplementary information to be show in the burger menu
+   */
+  auxiliaryDetails?: HeaderProps['auxiliaryDetails'];
+  /**
+   * Current language as an ISO code e.g. 'en-GB'
+   */
+  currentLang?: string;
+  /**
+   * The id for testing
+   */
+  dataTestId?: string;
+  /**
+   * Component to use for links. May be a default React <a /> or a custom component from the parent application
+   */
+  Link: (props: HeaderLinkProps) => JSX.Element;
+  /**
+   * `locale` - Object with localized strings
+   */
+  locale?: Locale;
+  /**
+   * Data representing the nav and sub nav
+   */
+  navigation: NavItem[];
+  /**
+   * Fn to set the active nav item
+   */
+  setActiveNavItem: (id: string | undefined) => void;
+  /**
+   * Tracking function to be called with tracking object, onClick for any link or button within the Header
+   */
+  trackingFn?: (trackingObj: HeaderTrackingObj) => void;
+}
+
+interface SubNavProps {
+  /**
+   * Is in Dropdown Menu display mode. from tablet-L breakpoint and up
+   */
+  isDropDownMenu: boolean;
+  /**
+   * Is the subnav currently open
+   */
+  isOpen: boolean;
+  /**
+   * Component to use for links. May be a default React <a /> or a custom component from the parent application
+   */
+  Link: (props: HeaderLinkProps) => JSX.Element;
+  /**
+   * `locale` - Object with localized strings
+   */
+  locale?: Locale;
+  /**
+   * The id of the parent nav item
+   */
+  navItemId?: string;
+  /**
+   * The label of the parent nav item
+   */
+  navItemName?: string;
+  /**
+   * Data representing the sub nav
+   */
+  subNavGroups: SubNavGroup[];
+  /**
+   * Tracking function to be called with tracking object, onClick for any link or button within the Header
+   */
+  trackingFn?: (trackingObj: HeaderTrackingObj) => void;
+}
+
+export { NavigationProps, NavItem, SubNavGroup, SubNavProps };
