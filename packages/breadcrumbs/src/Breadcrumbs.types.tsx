369f35c1bbac3096d8e4092d697eb75d41859871
Nathan Jamal <nathanjamal@ymail.com>
2023-05-24 11:40:42 +0100

    feat(breadcrumbs): add new breadcrumbs component
    add new breadcrumbs component
    
    HEYUI-292

---
+++ packages/breadcrumbs/src/Breadcrumbs.types.tsx
@@ -0,0 +1,19 @@
+export interface BreadcrumbLinkProps {
+  link?: string;
+  className?: string;
+  children: React.ReactNode;
+}
+export interface Breadcrumb {
+  link?: string;
+  title: string;
+}
+export interface BreadcrumbsProps {
+  breadcrumbs: Breadcrumb[];
+
+  /**
+   *  By default, the Breadcrumbs component uses a standard anchor element to build links. However you can pass a custom element to be used for all links.
+   */
+  LinkComponent?: (props: BreadcrumbLinkProps) => JSX.Element;
+  dataTestId?: string;
+  className?: string;
+}
