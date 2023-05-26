2cd53e7c0dae71438dff0b5cab90414f0e5b075a
Esra Altintas <esra.altintas@heycargroup.com>
2023-05-26 18:40:37 +0200

    feat(breadcrumbs): add seo breadcrumbList schema
    add seo breadcrumbList schema

--- packages/breadcrumbs/src/Breadcrumbs.tsx
+++ packages/breadcrumbs/src/Breadcrumbs.tsx
@@ -3,6 +3,7 @@ import cn from 'classnames';
 import { v4 as uuidv4 } from 'uuid';
 
 import useBreakpoint from './hooks/useBreakpoint.hook';
+import { LIST_ITEM_PROPS } from './Breadcrumbs.constants';
 import {
   Breadcrumb,
   BreadcrumbLinkProps,
@@ -56,20 +57,33 @@ const Breadcrumbs: FC<BreadcrumbsProps> = ({
     <ul
       className={cn(styles.breadcrumbWrapper, className)}
       data-test-id={dataTestId}
+      itemScope={true}
+      itemType="https://schema.org/BreadcrumbList"
     >
       {firstBreadcrumb?.link ? (
         <li
           className={styles.breadcrumbsList}
           key={`${firstBreadcrumb.title}-${0}`}
+          {...LIST_ITEM_PROPS}
         >
-          <Link className={styles.breadcrumbLink} link={firstBreadcrumb.link}>
+          <Link
+            className={styles.breadcrumbLink}
+            itemProp="item"
+            link={firstBreadcrumb.link}
+          >
             <span itemProp="name">{firstBreadcrumb.title}</span>
+            <meta content="1" itemProp="position"></meta>
           </Link>
         </li>
       ) : (
         firstBreadcrumb.title && (
-          <li className={styles.breadcrumbsList} key={firstBreadcrumb.title}>
+          <li
+            className={styles.breadcrumbsList}
+            key={firstBreadcrumb.title}
+            {...LIST_ITEM_PROPS}
+          >
             <span itemProp="name">{firstBreadcrumb.title}</span>
+            <meta content="1" itemProp="position"></meta>
           </li>
         )
       )}
@@ -83,22 +97,33 @@ const Breadcrumbs: FC<BreadcrumbsProps> = ({
             hiddenBreadcrumbs.map((breadcrumb, i) => {
               if (breadcrumb.link && i + 1 !== breadcrumbs.length) {
                 return (
-                  <li className={styles.breadcrumbsList} key={`${uuidv4()}`}>
+                  <li
+                    className={styles.breadcrumbsList}
+                    key={`${uuidv4()}`}
+                    {...LIST_ITEM_PROPS}
+                  >
                     <Link
                       className={styles.breadcrumbLink}
+                      itemProp="item"
                       link={breadcrumb.link}
                     >
                       <span itemProp="name">{breadcrumb.title}</span>
+                      <meta content={`${i + 2}`} itemProp="position"></meta>
                     </Link>
                   </li>
                 );
               }
 
               return (
-                <li className={styles.breadcrumbsList} key={breadcrumb.title}>
+                <li
+                  className={styles.breadcrumbsList}
+                  key={breadcrumb.title}
+                  {...LIST_ITEM_PROPS}
+                >
                   <span className={styles.disabled} itemProp="name">
                     {breadcrumb.title}
                   </span>
+                  <meta content={`${i + 2}`} itemProp="position"></meta>
                 </li>
               );
             })}
@@ -108,20 +133,24 @@ const Breadcrumbs: FC<BreadcrumbsProps> = ({
         <li
           className={styles.currentBreadcrumb}
           key={`${lastBreadcrumb.title}-${breadcrumbs.length - 1}`}
+          {...LIST_ITEM_PROPS}
         >
           <Link
             className={cn(styles.currentBreadcrumb, styles.breadcrumbLink)}
+            itemProp="item"
             link={lastBreadcrumb.link}
           >
             <span itemProp="name">{lastBreadcrumb.title}</span>
+            <meta content={`${breadcrumbs.length}`} itemProp="position"></meta>
           </Link>
         </li>
       ) : (
         lastBreadcrumb.title && (
-          <li key={lastBreadcrumb.title}>
+          <li key={lastBreadcrumb.title} {...LIST_ITEM_PROPS}>
             <span className={styles.currentBreadcrumb} itemProp="name">
               {lastBreadcrumb.title}
             </span>
+            <meta content={`${breadcrumbs.length}`} itemProp="position"></meta>
           </li>
         )
       )}
