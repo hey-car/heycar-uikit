4417cdc51979602797cad3e4e8753720ba91d615
nathan jamal <nathanjamal@ymail.com>
2023-06-08 15:46:38 +0100

    feat(slider): slider plus unit tests  (#216)
    * feat(slider): started on slider
    
    started
    
    HEYUI-354
    
    * feat(slider): slider plus unit tests
    
    working slider with unit tests
    
    HEYUI-354
    
    * remove unneeded files
    
    * added docs
    
    * added react-slider to packagejson plus docs updates
    
    * add state to canvas

---
+++ packages/slider/README.md
@@ -0,0 +1,47 @@
+# HeyCar-UIKit - Slider
+
+[![Tests](https://github.com/hey-car/heycar-uikit/actions/workflows/build.yml/badge.svg)](https://github.com/hey-car/heycar-uikit/actions/workflows/build.yml)
+[![Coverage Status](https://coveralls.io/repos/github/hey-car/heycar-uikit/badge.svg)](https://coveralls.io/github/hey-car/heycar-uikit)
+[![Demo build](https://github.com/hey-car/heycar-uikit/actions/workflows/main.yml/badge.svg)](https://github.com/hey-car/heycar-uikit/actions/workflows/main.yml)
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+
+Slider provides users with a way to interactively adjust a value or range within a specific range or set of options.
+
+## Installation
+
+To install and save in your package.json dependencies, run the command below using npm:
+
+```bash
+npm install @heycar-uikit/core
+
+# Or
+npm install @heycar-uikit/slider
+```
+
+## Usage
+
+```tsx
+// TSX/JSX file
+// Core import is recomended
+import Slider from '@heycar-uikit/core/slider';
+
+// or
+import Slider from '@heycar-uikit/slider';
+
+function App() {
+  return <Slider />;
+}
+```
+
+```css
+// CSS file
+// Core import is recomended
+@import '@heycar-uikit/core/slider/esm/Slider.css';
+
+// or
+@import '@heycar-uikit/slider/dist/esm/Slider.css';
+```
+
+## Documentation and sandbox
+
+[Storybook](https://hey-car.github.io/heycar-uikit/main/?path=/docs/components-molecules-slider--slider) documentation and sandbox
---
+++ packages/slider/src/Slider.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import ReactSlider from 'react-slider';
+
+import Typography from '@heycar-uikit/typography';
+
+import { DEFAULT_LOCALE } from './Slider.constants';
+import { SliderProps, ValueState } from './Slider.types';
+
+import styles from './styles/default.module.css';
+
+export const Slider = React.forwardRef<HTMLDivElement, SliderProps>(
+  (
+    {
+      dataTestId,
+      className,
+      onChange,
+      onAfterChange,
+      locale = DEFAULT_LOCALE,
+      minDistance,
+      stepCount,
+      selectedRangeIndexes,
+      ariaValueText,
+      showThumbLabels,
+      ...rest
+    },
+    ref,
+  ) => {
+    function removeDraggingClass() {
+      const activeThumb = document.getElementsByClassName(
+        'dragging',
+      )[0] as HTMLDivElement;
+
+      if (activeThumb) activeThumb.blur();
+    }
+
+    return (
+      <div
+        className={`${styles.sliderWrapper} ${className}`}
+        data-test-id={dataTestId}
+        ref={ref}
+        {...rest}
+      >
+        <ReactSlider
+          ariaLabel={[locale.thumb1, locale.thumb2]}
+          ariaValuetext={ariaValueText}
+          className={styles.heycarSlider}
+          max={stepCount - 1}
+          min={0}
+          minDistance={minDistance}
+          onAfterChange={(values: number[], thumbIndex: number) => {
+            removeDraggingClass();
+            if (onAfterChange)
+              onAfterChange([values[0], values[1]], thumbIndex);
+          }}
+          onChange={(value, thumbIndex) =>
+            onChange && onChange([value[0], value[1]], thumbIndex)
+          }
+          pearling={true}
+          renderThumb={(p: Record<string, any>, state: ValueState) => (
+            <div {...p}>
+              {ariaValueText && showThumbLabels && (
+                <Typography
+                  Component="span"
+                  className="tooltip"
+                  variant="caption5"
+                >
+                  {typeof ariaValueText === 'function'
+                    ? ariaValueText(state)
+                    : `${ariaValueText}${state.valueNow}`}
+                </Typography>
+              )}
+            </div>
+          )}
+          thumbActiveClassName="dragging"
+          thumbClassName="range-slider__thumb"
+          trackClassName="range-slider__track"
+          value={selectedRangeIndexes || [0, stepCount - 1]}
+        />
+      </div>
+    );
+  },
+);
+
+export default Slider;
